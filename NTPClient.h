ea200235c7049a06d4c3956199166bf551db3de8
Testato <Testato@users.noreply.github.com>
2017-07-23 09:05:51 +0300

    New default server
    https://github.com/arduino-libraries/NTPClient/issues/34

--- NTPClient.h
+++ NTPClient.h
@@ -13,7 +13,7 @@ class NTPClient {
     UDP*          _udp;
     bool          _udpSetup       = false;
 
-    const char*   _poolServerName = "time.nist.gov"; // Default time server
+    const char*   _poolServerName = "pool.ntp.org"; // Default time server
     int           _port           = NTP_DEFAULT_LOCAL_PORT;
     int           _timeOffset     = 0;
 
