1c341f7de6545bbccf983ca707f78cb3f6d01ec8
taranais <taranais@gmail.com>
2017-07-31 20:03:10 +0200

    define leap year

--- NTPClient.h
+++ NTPClient.h
@@ -7,6 +7,8 @@
 #define SEVENZYYEARS 2208988800UL
 #define NTP_PACKET_SIZE 48
 #define NTP_DEFAULT_LOCAL_PORT 1337
+#define LEAP_YEAR(Y)     ( (Y>0) && !(Y%4) && ( (Y%100) || !(Y%400) ) )
+
 
 class NTPClient {
   private:
