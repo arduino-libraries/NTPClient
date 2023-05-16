d1749704ce599ac51dd4c6873cbe710d821431e4
Hamza Yeşilmen <78810304+HamzaYslmn@users.noreply.github.com>
2023-05-17 01:44:01 +0300

    Update Esp32DateTime.ino
    esp8266 fix

--- examples/Basic/Esp32DateTime.ino
+++ examples/Basic/Esp32DateTime.ino
@@ -1,10 +1,9 @@
-#ifdef ESP32
-  #include <WiFi.h>
-#else
-  #include <ESP8266WiFi.h>
-#endif
-#include <WiFiUdp.h>
 #include <NTPClient.h>
+// change next line to use with another board/shield
+#include <ESP8266WiFi.h>
+//#include <WiFi.h> // for WiFi shield Or Esp32
+//#include <WiFi101.h> // for WiFi 101 shield or MKR1000
+#include <WiFiUdp.h>
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP);
